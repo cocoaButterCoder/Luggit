26e194f70376cbca099777c8a8deb39995f033fc
Bryan <cocoabuttercoder@gmail.com>
2019-06-09 11:15:39 -0400

    Added addItem() method

--- app/src/main/java/com/cocoabuttercoder/luggit/MainActivity.java
+++ app/src/main/java/com/cocoabuttercoder/luggit/MainActivity.java
@@ -1,15 +1,24 @@
- package com.cocoabuttercoder.luggit;
+package com.cocoabuttercoder.luggit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
- import com.cocoabuttercoder.luggit.R;
-
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addItem(){
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        EditText editText = findViewById(R.id.editText);
+        String itemName = editText.getText().toString();
+        Item item = new Item(itemName);
+
+        linearLayout.addView(item);
     }
 }
